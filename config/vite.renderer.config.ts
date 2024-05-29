86d00ffdac7ed0b5d02758d36ba6cc22d402ce23
Flavio Del Grosso <flavio.delgrosso@emeal.nttdata.com>
2024-05-29 09:59:51 +0200

    chore: update deps and vite configs to the latest versions

--- config/vite.renderer.config.ts
+++ config/vite.renderer.config.ts
@@ -1,20 +1,46 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { ConfigEnv, defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 
 import { productName, version } from '../package.json';
-
-import plugins from './vite.plugins.shared';
+import { checker } from 'vite-plugin-checker';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { pluginExposeRenderer } from './vite.base.config';
 
 // https://vitejs.dev/config
-export default defineConfig({
-  plugins: [react(), svgrPlugin(), ...plugins],
-  define: {
-    __DARWIN__: process.platform === 'darwin',
-    __WIN32__: process.platform === 'win32',
-    __LINUX__: process.platform === 'linux',
-    __APP_NAME__: JSON.stringify(productName),
-    __APP_VERSION__: JSON.stringify(version),
-    __DEV__: process.env.NODE_ENV === 'development',
-  },
+export default defineConfig((env) => {
+  const forgeEnv = env as ConfigEnv<'renderer'>;
+  const { root, mode, forgeConfigSelf } = forgeEnv;
+  const name = forgeConfigSelf.name ?? '';
+
+  return {
+    root,
+    mode,
+    base: './',
+    build: {
+      outDir: `.vite/renderer/${name}`,
+    },
+    resolve: {
+      preserveSymlinks: true,
+    },
+    clearScreen: false,
+    plugins: [
+      pluginExposeRenderer(name),
+      svgrPlugin(),
+      viteTsconfigPaths(),
+      checker({
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint "./**/*.{ts,tsx}"',
+        },
+      }),
+    ],
+    define: {
+      __DARWIN__: process.platform === 'darwin',
+      __WIN32__: process.platform === 'win32',
+      __LINUX__: process.platform === 'linux',
+      __APP_NAME__: JSON.stringify(productName),
+      __APP_VERSION__: JSON.stringify(version),
+      __DEV__: process.env.NODE_ENV === 'development',
+    },
+  };
 });
--- forge.config.ts
+++ forge.config.ts
@@ -1,9 +1,11 @@
 import path from 'path';
 
+import { FuseV1Options, FuseVersion } from '@electron/fuses';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
+import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import type { ForgeConfig } from '@electron-forge/shared-types';
 
@@ -18,7 +20,7 @@ const config: ForgeConfig = {
     // Set executable name
     executableName: productName,
     // Set application copyright
-    appCopyright: 'Copyright (C) 2023 Flavio Del Grosso',
+    appCopyright: 'Copyright (C) 2024 Flavio Del Grosso',
     // Set application icon
     icon: path.resolve(rootDir, 'assets/icons/icon'),
   },
@@ -45,6 +47,17 @@ const config: ForgeConfig = {
           config: 'config/vite.renderer.config.ts',
         },
       ],
+    }),
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
 };
