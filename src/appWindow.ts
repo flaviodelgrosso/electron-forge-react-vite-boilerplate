ea13cd91cef21887fff50da4886f23e696532947
Flavio Del Grosso <flaviodelgrosso@live.it>
2025-06-07 00:05:06 +0200

    fix: add missing fileURLToPath and __dirname definitions in appWindow.ts

--- src/appWindow.ts
+++ src/appWindow.ts
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { BrowserWindow, Menu, app } from 'electron';
 
 import windowStateKeeper from 'electron-window-state';
@@ -6,6 +7,9 @@ import windowStateKeeper from 'electron-window-state';
 import { registerMenuIpc } from 'src/ipc/menuIPC';
 import appMenu from 'src/menu/appMenu';
 import { registerWindowStateChangedEvents } from 'src/windowState';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 let appWindow: BrowserWindow;
 
